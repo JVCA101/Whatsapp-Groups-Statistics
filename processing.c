c751729e5b735214ab39cfd672c114f3a59c2c29
JVCA101 <joaovictordealmeid@gmail.com>
2025-11-23 12:09:53 -0300

    adding the start of processing in c

---
+++ processing.c
@@ -0,0 +1,63 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "dirent.h"
+#include "string.h"
+
+struct Person {
+    char* name;
+    long message_count;
+    long long character_count;
+};
+
+int process_dir(struct dirent *entry)
+{
+    // editing filename to be "folder/filename.txt"
+    char* txtname = entry->d_name;
+    strcat(txtname, "/");
+    strcat(txtname, entry->d_name);
+    txtname[strlen(txtname)-1] = '\0';
+    strcat(txtname, ".txt");
+    
+
+    FILE *text = fopen(txtname, "r");
+
+    if(text == NULL)
+    {
+        printf("ERROR: File %s could not be open\n", txtname);
+        // closedir(group_dir);
+        return EXIT_FAILURE;
+    }
+    printf("Processing file %s\n", txtname);
+
+    char c;
+    while((c = fgetc(text)) != EOF)
+        printf("%c", c);
+    fclose(text);
+    // closedir(group_dir);
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+    struct dirent *entry;
+    DIR *dr = opendir(".");
+    if(dr == NULL)
+    {
+	    printf("Could not open current directory\n");
+	    return EXIT_FAILURE;
+    }
+
+    while((entry = readdir(dr)) != NULL)
+	    if(entry->d_type == DT_DIR && entry->d_name[0] != '.')
+            if(process_dir(entry) == EXIT_FAILURE)
+            {
+                // printf("ERROR: process directory %s was not possible", entry->d_name);
+                break;
+            }
+
+    closedir(dr);
+
+    
+    return EXIT_SUCCESS;
+}
